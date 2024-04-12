98df63ea0ebc1dd6ae78fcd5c679153e4713d89f
Joury6a <jouryaltalib@gmail.com>
2024-04-12 10:13:21 +0300

    updated import statements, annotations, and add libraries

--- test/studentgrade/StudentGradeTest.java
+++ test/studentgrade/StudentGradeTest.java
@@ -1,14 +1,12 @@
 package studentgrade;
 
-import junit.framework.TestCase;
+import static org.junit.Assert.assertEquals;
+import org.junit.Test;
 
-/* JUnit 3.8.1 */
-public class StudentGradeTest extends TestCase {
-    
-    public StudentGradeTest(String testName) {
-        super(testName);
-    }
+/* JUnit 4.12 */
+public class StudentGradeTest {
 
+    @Test
     public void testGetGrade95() {
         System.out.println("getGrade 95");
         int mark = 95;
@@ -17,6 +15,7 @@ public class StudentGradeTest extends TestCase {
         assertEquals(expResult, result);
     }
 
+    @Test
     public void testGetGrade85() {
         System.out.println("getGrade 85");
         int mark = 85;
@@ -24,7 +23,8 @@ public class StudentGradeTest extends TestCase {
         String result = StudentGrade.getGrade(mark);
         assertEquals(expResult, result);
     }
-    
+
+    @Test
     public void testGetGrade75() {
         System.out.println("getGrade 85");
         int mark = 75;
